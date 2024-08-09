4714ba66e4ed08c571a5a1e67bcb244acfaa65c7
Beatriz Vasconcellos Espindola <165811496+beatrizve16@users.noreply.github.com>
2024-08-09 10:51:48 -0300

    Create olimpoiadas2.py

---
+++ olimpoiadas2.py
@@ -0,0 +1,61 @@
+import turtle
+
+pen = turtle.Turtle()
+pen.speed(100)
+
+pen.color('#0081C8')
+pen.pensize(5)
+pen.up()
+pen.setpos(-35, 95)
+pen.down()
+r = 50
+pen.circle(r,)
+
+
+pen.color('#000000')
+pen.pensize(5)
+pen.up()
+pen.setpos(80, 95)
+pen.down()
+r = 50
+pen.circle(r)
+
+pen.color('#EE334E')
+pen.pensize(5)
+pen.up()
+pen.setpos(195, 95)
+pen.down()
+r = 50
+pen.circle(r)
+
+pen.color('#FCB131')
+pen.pensize(5)
+pen.up()
+pen.setpos(25, 45)
+pen.down()
+r = 50
+pen.circle(r)
+
+pen.color('#00A651')
+pen.pensize(5)
+pen.up()
+pen.setpos(135, 45)
+pen.down()
+r = 50
+pen.circle(r)
+
+pen.color('#0081C8')
+pen.pensize(5)
+pen.up()
+pen.setpos(-35, 95)
+pen.down()
+r = 50
+pen.circle(r,120)
+
+
+
+# Hide the turtle cursor
+pen.hideturtle()
+
+# Keep the window open
+turtle.done()

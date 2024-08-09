56f8c09984299f5d19816a2d74fb6b54b66bad1c
Beatriz Vasconcellos Espindola <165811496+beatrizve16@users.noreply.github.com>
2024-08-09 09:28:08 -0300

    Create olimpiadas.py

---
+++ olimpiadas.py
@@ -0,0 +1,59 @@
+import turtle
+
+# Creating a turtle object(pen)
+pen = turtle.Turtle()
+pen.color('blue')
+pen.pensize(5)
+
+pen.up()
+pen.setpos(-35, 95)
+pen.down()
+
+r=50
+pen.circle(r)
+
+pen.color('black')
+pen.pensize(5)
+
+pen.up()
+pen.setpos(80, 95)
+pen.down()
+
+r=50
+pen.circle(r)
+
+pen.color('red')
+pen.pensize(5)
+
+pen.up()
+pen.setpos(195, 95)
+pen.down()
+
+r=50
+pen.circle(r)
+
+
+pen.color('yellow')
+pen.pensize(5)
+
+pen.up()
+pen.setpos(25, 45)
+pen.down()
+
+r=50
+pen.circle(r)
+
+
+
+pen.color('green')
+pen.pensize(5)
+
+pen.up()
+pen.setpos(135, 45)
+pen.down()
+
+r=50
+
+pen.circle(r)
+
+

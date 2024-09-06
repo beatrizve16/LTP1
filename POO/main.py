7b967517edc42d0dcab209be729dcc0dc097e72d
Beatriz Vasconcellos Espindola <165811496+beatrizve16@users.noreply.github.com>
2024-09-06 09:40:53 -0300

    Create main.py

---
+++ POO/main.py
@@ -0,0 +1,8 @@
+from carroClass import Carro
+
+carro1 = Carro('ford', 'bronco', 'azul',2024, 0 )
+carro1.acelerar()
+carro1.frenar()
+print(carro1.getModelo())
+carro1.setCor('Preto')
+print(carro1.getCor())

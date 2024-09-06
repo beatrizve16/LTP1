007215f1f56552cc7d5c51fd010a35e7e80b2e1a
Beatriz Vasconcellos Espindola <165811496+beatrizve16@users.noreply.github.com>
2024-09-06 09:40:16 -0300

    Create carroClass.py

---
+++ POO/carroClass.py
@@ -0,0 +1,23 @@
+class Carro(): #metodo contrutor
+    def __init__(self, marca, modelo, cor, ano, quilometragem):
+        self.marca = marca
+        self.modelo = modelo
+        self.cor = cor
+        self.ano = ano
+        self.quilometragem = quilometragem
+
+    def acelerar(self):
+        print('Vroom!')
+
+    def frenar(self):
+        print('carro parando!')
+
+
+    def getModelo(self):
+        return self.modelo
+
+    def setCor(self, novaCor):
+        self.cor = self.cor
+
+    def getCor(self):
+        return self.cor
